8f936be3a34ffae5320f28fac2c83e7746b5f415
Shashwat Pandey <47330116+shashwattpandeyy@users.noreply.github.com>
2021-09-01 11:13:38 +0530

    add push

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// this is used for api call
+const client = require('./connection.js')
+const express = require('express');
+const app = express();
+
+app.listen(3300, ()=>{
+    console.log("Sever is now listening at port 3300");
+})
+
+client.connect();
+
+app.get('/users', (req, res)=>{
+  client.query(`Select * from users`, (err, result)=>{
+      if(!err){
+          res.send(result.rows);
+
+      }
+  });
+  client.end;
+})
